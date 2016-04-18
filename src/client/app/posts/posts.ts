7fcd7a77d09aeae47e3e3f9909710f6483294ecf
Bob Roberts <broberts@socialwhirled.com>
2016-04-18 15:27:45 -0700

    updated to add in posts

---
+++ src/client/app/posts/posts.ts
@@ -0,0 +1,17 @@
+import {Component} from 'angular2/core';
+
+
+@Component({
+  selector: 'posts',
+  templateUrl: 'app//posts/posts.html',
+  styleUrls: ['app//posts/posts.css'],
+  providers: [],
+  directives: [],
+  pipes: []
+})
+
+export class Posts {
+
+  constructor() {}
+
+}
